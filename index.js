ghosh/hyper-solarized-dark

commit 3a71436a38758336fbe87505995f8f22725f7f00

duplicate-color-fix: Removed duplicate color
* Changed lightBlack from #586e75 to #002b36 to match what altercation/vim-colors-solarized expects it to be

index.js
@@ -4,7 +4,7 @@ const cursorColor = 'rgba(181, 137, 0, 0.6)'
 const borderColor = 'transparent'
 
 const colors = {
-  lightBlack:     '#586e75',
+  lightBlack:     '#002b36',
   black:          '#073642',
   lightGreen:     '#586e75',
   lightYellow:    '#657b83',

commit 2005653d1e8780698fcf600e527ead558c4c6221

Fix invisible lightBlack text
Fixes the issue described in https://github.com/ghosh/hyper-solarized-dark/issues/11 which was introduced in https://github.com/ghosh/hyper-solarized-dark/commit/3a71436a38758336fbe87505995f8f22725f7f00. 

The issue was that `backgroundColor = '#002b36'` was set to the same exact color as `lightBlack: '#002b36'`, making any text colored with `lightBlack` invisible. This commit reverses the changed introduced by the above commit.

index.js
@@ -4,7 +4,7 @@ const cursorColor = 'rgba(181, 137, 0, 0.6)'
 const borderColor = 'transparent'
 
 const colors = {
-  lightBlack:     '#002b36',
+  lightBlack:     '#586e75',
   black:          '#073642',
   lightGreen:     '#586e75',
   lightYellow:    '#657b83',